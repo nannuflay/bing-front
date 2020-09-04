bd178a931115fd7795536c540bd7dcda2717ddd1
simbaaf <mohameddcharafi@gmail.com>
2020-09-04 18:14:55 +0100

    add froala editor

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -1,16 +1,34 @@
 <template>
   <div >
     <HomeSeo></HomeSeo>
-   <Cards />
-  </div>
+    <Cards />
+    <div id="app" class="container mx-auto mt-20" style="height:500px">
+      <froala :tag="'textarea'" :config="config" v-model="model"></froala>  
+    </div>
+   </div>
 </template>
 
 <script>
 import HomeSeo from './components/HomeSeo'
 import Cards from './components/Cards'
+import VueFroala from 'vue-froala-wysiwyg';
+
 export default {
+  name: 'app',
   components: {
-    Cards, HomeSeo
+    Cards, HomeSeo 
+    },
+  data () {
+    return {
+      config: {
+        events: {
+          'froalaEditor.initialized': function () {
+            console.log('initialized')
+          }
+        }
+      },
+      model: ''
+    }
   }
 }
 </script>
