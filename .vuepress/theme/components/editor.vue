089365437b29959be063d62e30f60ec1daca49df
simbaaf <mohameddcharafi@gmail.com>
2020-09-09 22:44:05 +0100

    add more buttons toolbar #1

--- .vuepress/theme/components/editor.vue
+++ .vuepress/theme/components/editor.vue
@@ -10,15 +10,9 @@ export default {
   data () {
     return {
       config: {
-        events: {
-          'froalaEditor.initialized': function () {
-            console.log('initialized')
-            
-          },
-          'froalaEditor.size': function () {
-            height: 300
-          } 
-        }
+        placeholderText: 'Edit Your Content Here!',
+        height: 430,
+        toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'help', 'html', '|', 'undo', 'redo'],
       },
       model: ''
     }
--- .vuepress/theme/components/editorLayout.vue
+++ .vuepress/theme/components/editorLayout.vue
@@ -27,10 +27,9 @@
    </div>
 
     <div class=" container mx-auto" style="margin-top:90px">
-      <span class=" text-grey-darkest text-xl font-extrabold my-3">Editor text:</span>
+      <span class=" text-grey-darkest text-xl font-extrabold my-3">Editor text</span>
       <p class=" text-grey-dark text-sm my-3">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptas iste ea. Eum ad voluptatum ratione rerum assumenda,
-          consequuntur repellendus vel adipisci, voluptatibus dolores suscipit neque id debitis, ullam veniam!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptas iste ea.
         </p>
       <Editor class="mt-6"/>
     </div>
